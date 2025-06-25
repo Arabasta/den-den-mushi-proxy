30bb64f066fc204dfe25bdc31151a7f3d46cff06
Arabasta <keiyamx@gmail.com>
2025-06-25 14:59:03 +0800

    feat: add blocked protocol header

--- internal/websocket/protocol/header.go
+++ internal/websocket/protocol/header.go
@@ -12,21 +12,22 @@ const (
 	// Error for errors from the proxy server
 	Error Header = 0x02
 
+	// Blocked for blocked messages from the proxy server, e.g., "You are not allowed to run su -"
+	Blocked Header = 0x03
+
 	// Warn for warnings from the proxy server, e.g., "10 minutes left on your session"
-	Warn Header = 0x03
+	Warn Header = 0x04
 
 	// Broadcast for broadcasting messages to all connected clients, e.g., "Server is going down for maintenance"
-	Broadcast Header = 0x04
+	Broadcast Header = 0x05
 
-	Reserved3 Header = 0x05
+	Reserved3 Header = 0x06
 
-	Reserved4 Header = 0x06
+	Reserved4 Header = 0x07
 
-	Reserved5 Header = 0x07
+	Reserved5 Header = 0x08
 
-	Reserved6 Header = 0x08
-
-	Reserved7 Header = 0x09
+	Reserved6 Header = 0x09
 
 	// Resize called when the terminal is resized from the client
 	Resize Header = 0x10
