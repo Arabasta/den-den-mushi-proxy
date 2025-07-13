f76e8e5344147a933caa74dfe1c0977f3f265359
Arabasta <keiyamx@gmail.com>
2025-07-13 23:47:13 +0800

    line editor add mutex

--- internal/proxy/filter/line_editor.go
+++ internal/proxy/filter/line_editor.go
@@ -1,16 +1,24 @@
 package filter
 
+import (
+	"sync"
+)
+
 const (
 	MaxBufferLength = 8192
 	ErrorMaxBuffer  = "???Error Max Buffer"
 )
 
 type LineEditor struct {
+	mu     sync.RWMutex
 	Buffer []rune
 	Cursor int
 }
 
 func (e *LineEditor) Insert(r rune) {
+	e.mu.Lock()
+	defer e.mu.Unlock()
+
 	if len(e.Buffer) >= MaxBufferLength {
 		e.Buffer = []rune(ErrorMaxBuffer)
 		e.Cursor = len(e.Buffer)
@@ -26,6 +34,9 @@ func (e *LineEditor) Insert(r rune) {
 }
 
 func (e *LineEditor) Backspace() {
+	e.mu.Lock()
+	defer e.mu.Unlock()
+
 	if e.Cursor > 0 && len(e.Buffer) > 0 {
 		e.Buffer = append(e.Buffer[:e.Cursor-1], e.Buffer[e.Cursor:]...)
 		e.Cursor--
@@ -33,22 +44,34 @@ func (e *LineEditor) Backspace() {
 }
 
 func (e *LineEditor) MoveLeft() {
+	e.mu.Lock()
+	defer e.mu.Unlock()
+
 	if e.Cursor > 0 {
 		e.Cursor--
 	}
 }
 
 func (e *LineEditor) MoveRight() {
+	e.mu.Lock()
+	defer e.mu.Unlock()
+
 	if e.Cursor < len(e.Buffer) {
 		e.Cursor++
 	}
 }
 
 func (e *LineEditor) String() string {
+	e.mu.RLock()
+	defer e.mu.RUnlock()
+
 	return string(e.Buffer)
 }
 
 func (e *LineEditor) Reset() {
+	e.mu.Lock()
+	defer e.mu.Unlock()
+
 	e.Buffer = nil
 	e.Cursor = 0
 }
