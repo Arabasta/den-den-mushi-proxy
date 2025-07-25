a0f30e0f3ca19535a6ee1dc76183d8920c1e0aaa
Arabasta <keiyamx@gmail.com>
2025-07-25 17:13:04 +0800

    fix: puppet task param

--- internal/proxy/integrations/puppet/injectTask.go
+++ internal/proxy/integrations/puppet/injectTask.go
@@ -4,7 +4,14 @@ import (
 	"den-den-mushi-Go/pkg/dto"
 )
 
-type injectKeyParams struct {
+type taskBody struct {
+	Environment string           `json:"environment"`
+	Task        string           `json:"task"`
+	Params      sshKeyTaskParams `json:"params"`
+	Scope       taskScope        `json:"scope"`
+}
+
+type sshKeyTaskParams struct {
 	PublicKey   string `json:"public_key"`
 	ServerIP    string `json:"server_ip"`
 	OSUser      string `json:"os_user"`
@@ -12,13 +19,24 @@ type injectKeyParams struct {
 	ConnType    string `json:"conn_type"`
 }
 
+type taskScope struct {
+	Nodes []string `json:"nodes"`
+}
+
 func (p *Client) KeyInject(publicKey string, conn dto.Connection) error {
-	params := injectKeyParams{
-		PublicKey:   publicKey,
-		ServerIP:    conn.Server.IP,
-		OSUser:      conn.Server.OSUser,
-		ConnPurpose: string(conn.Purpose),
-		ConnType:    string(conn.Type),
+	params := taskBody{
+		Environment: "production",
+		Task:        "task",
+		Params: sshKeyTaskParams{
+			PublicKey:   publicKey,
+			ServerIP:    conn.Server.IP,
+			OSUser:      conn.Server.OSUser,
+			ConnPurpose: string(conn.Purpose),
+			ConnType:    string(conn.Type),
+		},
+		Scope: taskScope{
+			Nodes: []string{conn.Server.IP},
+		},
 	}
 
 	_, err := p.callPuppetTask(TaskInjectPublicKey, params)
--- internal/proxy/integrations/puppet/removeTask.go
+++ internal/proxy/integrations/puppet/removeTask.go
@@ -4,21 +4,20 @@ import (
 	"den-den-mushi-Go/pkg/dto"
 )
 
-type removeKeyParams struct {
-	PublicKey   string `json:"public_key"`
-	ServerIP    string `json:"server_ip"`
-	OSUser      string `json:"os_user"`
-	ConnPurpose string `json:"conn_purpose"`
-	ConnType    string `json:"conn_type"`
-}
-
 func (p *Client) KeyRemove(publicKey string, conn dto.Connection) error {
-	params := removeKeyParams{
-		PublicKey:   publicKey,
-		ServerIP:    conn.Server.IP,
-		OSUser:      conn.Server.OSUser,
-		ConnPurpose: string(conn.Purpose),
-		ConnType:    string(conn.Type),
+	params := taskBody{
+		Environment: "production",
+		Task:        "task",
+		Params: sshKeyTaskParams{
+			PublicKey:   publicKey,
+			ServerIP:    conn.Server.IP,
+			OSUser:      conn.Server.OSUser,
+			ConnPurpose: string(conn.Purpose),
+			ConnType:    string(conn.Type),
+		},
+		Scope: taskScope{
+			Nodes: []string{conn.Server.IP},
+		},
 	}
 
 	_, err := p.callPuppetTask(TaskRemovePublicKey, params)
