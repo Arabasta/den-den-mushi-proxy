0d980e01daf942c0682f9f2b97acf986ecefde64
Arabasta <keiyamx@gmail.com>
2025-07-28 19:04:50 +0800

    update puppet task

--- internal/proxy/integrations/puppet/task.go
+++ internal/proxy/integrations/puppet/task.go
@@ -20,10 +20,7 @@ type taskBody struct {
 
 type sshKeyTaskParams struct {
 	PublicKey    string `json:"public_key"`
-	ServerIP     string `json:"server_ip"`
 	OSUser       string `json:"os_user"`
-	ConnPurpose  string `json:"conn_purpose"`
-	ConnType     string `json:"conn_type"`
 	ConnectionID string `json:"connection_id"`
 }
 
