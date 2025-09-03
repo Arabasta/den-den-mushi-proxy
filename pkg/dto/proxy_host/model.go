99282be72a936deb30d5c6ced2902e044c184fab
Arabasta <keiyamx@gmail.com>
2025-09-03 22:06:56 +0800

    feat: add new model for proxy host

--- pkg/dto/proxy_host/model.go
+++ pkg/dto/proxy_host/model.go
@@ -1,6 +1,9 @@
 package proxy_host
 
-import "den-den-mushi-Go/pkg/types"
+import (
+	"den-den-mushi-Go/pkg/types"
+	"time"
+)
 
 type Model struct {
 	HostName    string      `gorm:"column:HostName;primaryKey;size:191"`
@@ -17,3 +20,46 @@ type Model struct {
 func (Model) TableName() string {
 	return "ddm_proxy_hosts"
 }
+
+type Model2 struct {
+	HostName  string      `gorm:"column:HostName;primaryKey;size:191"`
+	ProxyType types.Proxy `gorm:"column:ProxyType;size:191"`
+
+	Url         string           `gorm:"column:Url;size:191;not null;unique"`
+	IpAddress   string           `gorm:"column:IpAddress;size:191"`
+	Environment ProxyEnvironment `gorm:"column:Environment;size:191;not null;default:'development'"`
+	Country     string           `gorm:"column:Country;size:191;not null;default:'unknown'"`
+
+	LastHeartbeatAt time.Time `gorm:"column:LastHeartbeatAt;type:timestamp"`
+
+	DrainingStartAt *time.Time      `gorm:"column:DrainingStartAt;type:timestamp;nullable;default:NULL"`
+	DrainingEndAt   *time.Time      `gorm:"column:DrainingEndAt;type:timestamp;nullable;default:NULL"`
+	DeploymentColor DeploymentColor `gorm:"column:DeploymentColor;size:191;not null;default:'blue'"`
+
+	MaxSessions    int `gorm:"column:MaxSessions;default:100"`
+	ActiveSessions int `gorm:"column:ActiveSessions;default:0"`
+}
+
+func (Model2) TableName() string {
+	return "ddm_proxies"
+}
+
+type ProxyEnvironment string
+
+const (
+	Development ProxyEnvironment = "development"
+	UAT         ProxyEnvironment = "uat"
+	Production  ProxyEnvironment = "production"
+)
+
+type DeploymentColor string
+
+const (
+	// AllDeployment only used for control to indicate that all deployments are being considered
+	AllDeployment DeploymentColor = "all"
+
+	BlueDeployment   DeploymentColor = "blue"
+	GreenDeployment  DeploymentColor = "green"
+	RedDeployment    DeploymentColor = "red"
+	YellowDeployment DeploymentColor = "yellow"
+)
---
+++ scripts/ddm_proxies.sql
@@ -0,0 +1,5 @@
+INSERT INTO ddm_proxies
+    (HostName, ProxyType, Url, IpAddress, Environment, Country)
+    VALUES (
+    'Kes-MacBook-Pro.local', 'OS', 'http://localhost:45007',
+            '127.0.0.1', 'Development', 'SG');
